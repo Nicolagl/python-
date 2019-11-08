7446a3793fbd8ef357ef0b8c162bb986aea68dba
Nicolagl <564152009@qq.com>
2019-11-08 17:50:38 +0800

    11.8

---
+++ sbwht.py
@@ -0,0 +1,14 @@
+print("1".center(20))
+print("1 1".center(20))
+print("1 2 1".center(20))
+print("1 3 3 1".center(20))
+print("1 4 6 4 1".center(20))
+
+day=8
+taozi=1
+while(day>0):
+    print("第%d天桃子数为: %d"%(day,taozi))
+    taozi=(taozi+1)*2
+    day-=1
+print(sn,an*pow(0.5,n-1))
+
