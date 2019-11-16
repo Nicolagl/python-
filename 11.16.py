e697ccfc932047367f67165a58bc05476f2b2bfa
Nicolagl <564152009@qq.com>
2019-11-17 00:00:58 +0800

    11.17

---
+++ 11.16.py
@@ -0,0 +1,9 @@
+#n1,n2,n3=eval(input ("input 3 w:"))
+
+
+#aver=(n1+n2+n3)/3
+#print("aver is",aver,n1,n2,n3)
+a=22.5
+a=round(a)
+print(a)
+#round四舍五入
--- a.py
+++ a.py
@@ -1,3 +1,6 @@
+#wstssb
+
+
 a=float(input("输入"))
 
 print(str.format("共75位左空格补齐{0:75.2f}",a))
