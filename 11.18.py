6b431ce075dda6ac3e13d12747addf14ea367697
Nicolagl <564152009@qq.com>
2019-11-18 13:42:36 +0800

    if for func

---
+++ 11.18.py
@@ -0,0 +1,14 @@
+count=0
+while count<3:
+    print("whtssb")
+    count=count+1
+i=5
+print("i=",i)
+for i in range(0,4,2):
+    print("wanghantangsdsb",i)#i=0,i<4,i+2
+def max(num1,mum2):
+    if num1>mum2 :
+        return mum1
+    else:
+        return mum2
+print("max",max(5,7))
