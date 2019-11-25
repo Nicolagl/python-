b580b5b11f3f54ce12630923a4cfd844d5c2a67e
Nicolagl <564152009@qq.com>
2019-11-26 00:05:26 +0800

    11.25.b

--- 11.25.py
+++ 11.25.py
@@ -3,7 +3,18 @@ def linearsearch(lst,key):
         if key == lst[i]:
             return i
     return -1
+def insertsort(lst):
+    for i in range(1,len(lst)):
+        currentelement=lst[i]
+        k=i-1
+        while k>=0 and lst[k]>currentelement:
+            lst[k+1]=lst[k]
+            k-=1
+        lst[k+1]=currentelement
+
 list1=[]
 for i in range(5):
-    list1.append(int(input()))
+    list1.append(int(input("请您丫输个数")))
 print(linearsearch(list1,5))
+insertsort(list1)
+print(list1)
