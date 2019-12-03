26910e307da96082273ae6dcf3b7c37dafb6ae1f
Nicolagl <564152009@qq.com>
2019-12-03 11:38:20 +0800

    12.3

---
+++ 12.3.py
@@ -0,0 +1,10 @@
+tuple1=("green","red","blue")
+print(tuple1)
+tuple2=tuple([7,8,5,4,5,6,2])
+print(tuple2)
+
+print(len(tuple2),sum(tuple2))#tuple2[0])
+
+list1=list(tuple2)
+list1.sort()
+print(list1)
