c5f9806ae2c2b635da08d37eb66d9732c1b71169
Nicolagl <564152009@qq.com>
2019-12-07 14:46:07 +0800

    11.7s

--- line.py
+++ line.py
@@ -7,3 +7,10 @@ node1=Node(5)
 node2=Node(4,node1)
 
 print(node1.data,node2.data,"next is",node1.next,"node2 next",node2.next.data)
+
+head=None
+for i in range(1,5):
+    head=Node(i,head)
+while head!=None:
+    print(head.data)
+    head=head.next
