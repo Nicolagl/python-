fadc558e55f8f306cb5ac21e71a43452bbb11e81
Nicolagl <564152009@qq.com>
2019-12-16 14:19:34 +0800

    12.16

--- MyApplication/app/src/main/java/com/example/myapplication/Main2Activity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -1,14 +1,55 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "you clicked add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "you clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fist_layout);
+
+        Button buttom1=(Button) findViewById(R.id.wht);
+        buttom1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(Main2Activity.this,"王汉唐是哨兵",Toast.LENGTH_SHORT).show();
+            }
+
+
+
+        });
+
     }
 }
